c94ec926da9169249d0da5a2c785763bc8b27f68
deivid1801 <deividleonardo332@gmail.com>
2023-05-24 13:00:52 -0300

    dd

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {livros} from '@/_data/livro'
+
+</script>
+
+<template>
+  <h1><mark style="background-color: rgb(190, 2, 39);color: rgb(0, 0, 0);">Minha Livraria</mark></h1>
+  <div class="listagem-livros">
+    <div v-for="livro in livros" :key="livro.id" class="card-livro">
+      <img :src="livro.cover" alt="capa" class="livro-capa" />
+      <p class="livro-titulo">{{ livro.title }}</p>
+      <p class="livro-autor">{{ livro.author }}</p>
+      <p class="livro-preco">{{ livro.price }}</p>
+
+    </div>
+
+ </div>
+  
+</template>
+
+<style scoped>
+.livro-titulo{
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-bottom: 5px;
+}
+.livro-capa{
+  width: 200px;
+}
+.card-livro{
+  background-color:beige;
+  padding: 10px;
+  border-radius: 10px;
+  width: 200px;
+  margin: 10px;
+}
+.listagem-livros {
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+</style>
---
+++ src/_data/livro.js
@@ -0,0 +1,51 @@
+export const livros = [
+    {
+      id: 1,
+      title: "O Senhor dos Anéis: a sociedade do anel",
+      author: 'J. R. R. Tolkien',
+      price: 39.9,
+      cover: 'https://m.media-amazon.com/images/I/A16GGot7vHL._AC_UL400_.jpg',
+    },
+    {
+      id: 2,
+      title: "O senhor dos Anéis: as duas torres",
+      author: 'J. R. R. Tolkien',
+      price: 39.9,
+      cover: 'https://m.media-amazon.com/images/I/81N9hxO6XdL._AC_UL400_.jpg',
+    },
+    {
+      id: 3,
+      title: "Cristiano Ronaldo: Biograph",
+      author: 'C. R. 7.',
+      price: 49.9,
+      cover: 'https://m.media-amazon.com/images/I/41VHgYCqnqL._AC_UL400_.jpg',
+    },
+    {
+        id: 4,
+        title: "Tentação Sombria",
+        author: 'G. Benevidles',
+        price: 20.49,
+        cover: 'https://m.media-amazon.com/images/I/51jeC8c2KyL.jpg'
+    },
+    {
+        id: 5,
+        title: "O planeta Neymar",
+        author: 'P. Vinicius Coelho',
+        price: 22.90,
+        cover: 'https://m.media-amazon.com/images/I/51UhCGROXoL.jpg'
+    },
+    {
+        id: 6,
+        title: "The last Kingdom",
+        author: 'Bernard Cornwell',
+        price: 22.90,
+        cover: 'https://m.media-amazon.com/images/I/51GfH25lbSL.jpg'
+    },
+    {
+        id: 7,
+        title: "Naruto Itachi",
+        author: 'M. Kishimoto, T. Yano',
+        price: 25.19,
+        cover: 'https://m.media-amazon.com/images/I/51Sw3Vyyg5L.jpg'
+    }
+  ]
